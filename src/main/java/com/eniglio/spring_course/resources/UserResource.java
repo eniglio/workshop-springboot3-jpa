5566470f589989cc16f18a28b1fa25a2f3b5546d
eniglio <eniglio@hotmail.com>
2025-06-09 20:14:47 -0300

    User insert

--- src/main/java/com/eniglio/spring_course/resources/UserResource.java
+++ src/main/java/com/eniglio/spring_course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.eniglio.spring_course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.eniglio.spring_course.entities.User;
 import com.eniglio.spring_course.services.UserService;
@@ -32,5 +36,14 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 		
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		//retorna 201
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
